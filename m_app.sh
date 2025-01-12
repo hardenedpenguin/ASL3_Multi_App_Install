9b8fea57a021aea5395ba2538cb29e810fe7f5ef
Jory Pratt <anarchy@gentoo.org>
2025-01-11 18:46:32 -0600

    Rewrite m_apps to support user option as to what packages are installed
    Signed-off-by: Jory Pratt <anarchy@gentoo.org>

--- m_app.sh
+++ m_app.sh
@@ -1,145 +1,177 @@
-#!/bin/bash
+#!/bin/sh
 
-# This Script file was created to assist with a new install of AllStarLink version 3. 
-# Once you have applied your node setup you can run this file to Install SkywarnPlus, Apache2, AllScan Dashboard
-# DVSwitch Server, Supermon 7.4+ Fresh Install and Upgradeable Install version of Supermon 7.4+ 
-
-# This file created by Freddie Mac - KD5FMU with the help of ChatGPT with help from Allan - WA3WCO
-
+# This script assists with the new install of AllStarLink version 3.
+# It installs SkywarnPlus, AllScan Dashboard, DVSwitch Server, and Supermon 7.4+.
 #
-# check if root
+# Copyright (C) 2024 Freddie Mac - KD5FMU 
+# Copyright (C) 2024 Allan - OCW3AW
+# Copyright (C) 2024 Jory A. Pratt - W5GLE
 #
-SUDO=""
-if [[ $EUID != 0 ]]; then
-    SUDO="sudo"
-    SUDO_EUID=$(${SUDO} id -u)
-    if [[ ${SUDO_EUID} -ne 0 ]]; then
-        echo "This script must be run as root or with sudo"
-        exit 1
-    fi
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+# Check if root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root or with sudo"
+    exit 1
 fi
 
-# Install package dependencies for DVSwitch
-echo "Package Depend for DVSwitch..."
-sudo apt install php-cgi libapache2-mod-php8.2
-echo "Apache2 for DVSwitch Installed..."
-
-# Install SkywarnPlus
-echo "Installing SkywarnPlus..."
-cd ~
-sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/Mason10198/SkywarnPlus/main/swp-install)"
-echo "SkywarnPlus installed."
-
-# Install the AllScan dashboard
-echo "Installing AllScan..."
-cd ~
-sudo wget 'https://raw.githubusercontent.com/davidgsd/AllScan/main/AllScanInstallUpdate.php'
-sudo chmod 755 AllScanInstallUpdate.php
-sudo ./AllScanInstallUpdate.php
-echo "AllScan installed."
-
-# Install DVSwitch Server
-echo "Installing DVSwitch Server..."
-cd ~
-sudo wget dvswitch.org/bookworm
-sudo chmod +x bookworm
-sudo ./bookworm
-sudo apt update
-sudo apt install -y dvswitch-server
-echo "DVSwitch Server installed."
-
-# Install Supermon 7.4
-echo "Installing Supermon 7.4..."
-cd /usr/local/sbin
-sudo wget "http://2577.asnode.org:43856/supermonASL_fresh_install" -O supermonASL_fresh_install
-sudo chmod +x supermonASL_fresh_install
-hash
-sudo ./supermonASL_fresh_install
-echo "Supermon 7.4 installed."
-
-# Install Supermon 7.4 Upgradeable
-echo "Installing Upgradeable Supermon 7.4..."
-cd /usr/local/sbin
-sudo wget "http://2577.asnode.org:43856/supermonASL_latest_update" -O supermonASL_latest_update
-sudo chmod +x supermonASL_latest_update
-hash
-sudo ./supermonASL_latest_update
-echo "Upgradeable Supermon 7.4 installed."
-
-# Path to the rpt.conf file
 CONF_FILE="/etc/asterisk/rpt.conf"
 
-# Backup the original configuration file
-cp $CONF_FILE ${CONF_FILE}.bak
+# Function to display usage
+usage() {
+	cat << EOF
+Usage: $0 [OPTIONS]
 
-# Add line to rpt.conf
-echo "Updating rpt.conf..."
-sudo sed -i '/\[functions\]/a SMUPDATE=cmd,/usr/local/sbin/supermonASL_latest_update' /etc/asterisk/rpt.conf
-echo "rpt.conf updated."
+Options:
+  -a    Install allscan
+  -s    Install supermon
+  -w    Install skywarnplus
+  -d    Install dvswitch
+  -h    Display this help message
 
-# Add line to rpt.conf for SkywarnPlus
-echo "Updating rpt.conf..."
-sudo sed -i '/\[functions\]/a ;SkyControl DTMF commands' /etc/asterisk/rpt.conf
-echo "rpt.conf updated."
+You can combine options to install multiple software (e.g., $0 -a -s).
+EOF
+}
 
-# Update rpt.conf to add SkywarnPlus control commands
-echo "Updating rpt.conf with SkywarnPlus commands..."
-sudo sed -i '/\[functions\]/a \
-831 = cmd,/usr/local/bin/SkywarnPlus/SkyControl.py enable toggle ; Toggles SkywarnPlus\n\
-832 = cmd,/usr/local/bin/SkywarnPlus/SkyControl.py sayalert toggle ; Toggles SayAlert\n\
-833 = cmd,/usr/local/bin/SkywarnPlus/SkyControl.py sayallclear toggle ; Toggles SayAllClear\n\
-834 = cmd,/usr/local/bin/SkywarnPlus/SkyControl.py tailmessage toggle ; Toggles TailMessage\n\
-835 = cmd,/usr/local/bin/SkywarnPlus/SkyControl.py courtesytone toggle ; Toggles CourtesyTone\n\
-836 = cmd,/usr/local/bin/SkywarnPlus/SkyControl.py alertscript toggle ; Toggles AlertScript\n\
-837 = cmd,/usr/local/bin/SkywarnPlus/SkyControl.py idchange toggle ; Toggles IDChange\n\
-838 = cmd,/usr/local/bin/SkywarnPlus/SkyControl.py changect normal ; Forces CT to "normal" mode\n\
-839 = cmd,/usr/local/bin/SkywarnPlus/SkyControl.py changeid normal ; Forces ID to "normal" mode' /etc/asterisk/rpt.conf
-echo "rpt.conf updated with SkywarnPlus commands."
+# Functions to install each software package
+install_allscan() {
+	echo "Installing allscan..."
+	# First make sure required deps are installed
+	apt install php unzip -y
+	cd /tmp || exit
+	wget 'https://raw.githubusercontent.com/davidgsd/AllScan/main/AllScanInstallUpdate.php'
+	chmod 755 AllScanInstallUpdate.php
+	./AllScanInstallUpdate.php
+	echo "Allscan installation complete."
+}
 
-# Add SkyDescribe DTMF commands to rpt.conf
-echo "Adding SkyDescribe DTMF commands to rpt.conf..."
-sudo sed -i '/\[functions\]/a \
-841 = cmd,/usr/local/bin/SkywarnPlus/SkyDescribe.py 1 ; SkyDescribe the 1st alert\n\
-842 = cmd,/usr/local/bin/SkywarnPlus/SkyDescribe.py 2 ; SkyDescribe the 2nd alert\n\
-843 = cmd,/usr/local/bin/SkywarnPlus/SkyDescribe.py 3 ; SkyDescribe the 3rd alert\n\
-844 = cmd,/usr/local/bin/SkywarnPlus/SkyDescribe.py 4 ; SkyDescribe the 4th alert\n\
-845 = cmd,/usr/local/bin/SkywarnPlus/SkyDescribe.py 5 ; SkyDescribe the 5th alert\n\
-846 = cmd,/usr/local/bin/SkywarnPlus/SkyDescribe.py 6 ; SkyDescribe the 6th alert\n\
-847 = cmd,/usr/local/bin/SkywarnPlus/SkyDescribe.py 7 ; SkyDescribe the 7th alert\n\
-848 = cmd,/usr/local/bin/SkywarnPlus/SkyDescribe.py 8 ; SkyDescribe the 8th alert\n\
-849 = cmd,/usr/local/bin/SkywarnPlus/SkyDescribe.py 9 ; SkyDescribe the 9th alert' /etc/asterisk/rpt.conf
-echo "SkyDescribe DTMF commands added to rpt.conf."
+install_supermon() {
+	echo "Installing supermon..."
+	# Install deps before we fetch script and run
+	apt -y install apache2 php libapache2-mod-php libcgi-session-perl bc
+	cd /tmp || exit
+	wget "http://2577.asnode.org:43856/supermonASL_fresh_install" -O supermonASL_fresh_install
+	chmod +x supermonASL_fresh_install
+	./supermonASL_fresh_install
+	echo "Supermon 7.4+ fresh installation complete."
+	
+	echo "Installing Upgradeable Supermon 7.4..."
+	wget "http://2577.asnode.org:43856/supermonASL_latest_update" -O supermonASL_latest_update
+	chmod +x supermonASL_latest_update
+	./supermonASL_latest_update
 
-# Uncomment and change tailmessagetime and tailsquashedtime
-sed -i '/tailmessagetime=/s/^#//; s/tailmessagetime=.*/tailmessagetime=600000/' $CONF_FILE
-sed -i '/tailsquashedtime=/s/^#//; s/tailsquashedtime=.*/tailsquashedtime=30000/' $CONF_FILE
+	cp "$CONF_FILE" "${CONF_FILE}.bak-supermon"
+	sed -i '/\[functions\]/a SMUPDATE=cmd,/usr/local/sbin/supermonASL_latest_update' "$CONF_FILE"
 
-# Insert tailmessagelist line before tailsquashedtime
-awk '/tailsquashedtime=30000/ { print "tailmessagelist = /tmp/SkywarnPlus/wx-tail"; } { print }' $CONF_FILE > ${CONF_FILE}.tmp && mv ${CONF_FILE}.tmp $CONF_FILE
-echo "Modifications applied successfully."
+	echo "Setting up cron job..."
+	CRON_COMMENT="# Supermon 7.4 updater crontab entry"
+	CRON_JOB="0 3 * * * /var/www/html/supermon/astdb.php cron"
+	( crontab -l 2>/dev/null; echo "$CRON_COMMENT"; echo "$CRON_JOB" ) | crontab -
+	echo "Cron job set."
 
-# Must allow apache2 to access /tmp for info to be displayed properly on dvswitch dashboard
-sudo cp /lib/systemd/system/apache2.service /etc/systemd/system/
-sudo sed -i 's/true/false/g' /etc/systemd/system/apache2.service
-sudo systemctl restart apache2
+	echo "Upgradeable Supermon 7.4 install complete."
+}
 
-# Add cron job
-# echo "Setting up cron job..."
-# (sudo crontab -l 2>/dev/null; echo "0 3 * * * /var/www/html/supermon/astdb.php cron") | sudo crontab -
-# echo "Cron job set."
+install_skywarnplus() {
+	echo "Installing skywarnplus..."
+	# Install deps before we install skywarn via script
+	apt install -y unzip python3 python3-pip ffmpeg python3-ruamel.yaml python3-requests python3-dateutil python3-pydub
+	cd /tmp || exit
+	bash -c "$(curl -fsSL https://raw.githubusercontent.com/Mason10198/SkywarnPlus/main/swp-install)"
 
-# Define the cron job and its preceding comment
-echo "Setting up cron job..."
-CRON_COMMENT="# Supermon 7.4 updater crontab entry"
-CRON_JOB="0 3 * * * /var/www/html/supermon/astdb.php cron"
+	cp "$CONF_FILE" "${CONF_FILE}.bak-skywarn"
+	sed -i '/\[functions\]/a \
+831 = cmd,/usr/local/bin/SkywarnPlus/SkyControl.py enable toggle ; Toggles SkywarnPlus \
+832 = cmd,/usr/local/bin/SkywarnPlus/SkyControl.py sayalert toggle ; Toggles SayAlert \
+833 = cmd,/usr/local/bin/SkywarnPlus/SkyControl.py sayallclear toggle ; Toggles SayAllClear \
+834 = cmd,/usr/local/bin/SkywarnPlus/SkyControl.py tailmessage toggle ; Toggles TailMessage \
+835 = cmd,/usr/local/bin/SkywarnPlus/SkyControl.py courtesytone toggle ; Toggles CourtesyTone \
+836 = cmd,/usr/local/bin/SkywarnPlus/SkyControl.py alertscript toggle ; Toggles AlertScript \
+837 = cmd,/usr/local/bin/SkywarnPlus/SkyControl.py idchange toggle ; Toggles IDChange \
+838 = cmd,/usr/local/bin/SkywarnPlus/SkyControl.py changect normal ; Forces CT to "normal" mode \
+839 = cmd,/usr/local/bin/SkywarnPlus/SkyControl.py changeid normal ; Forces ID to "normal" mode \
+841 = cmd,/usr/local/bin/SkywarnPlus/SkyDescribe.py 1 ; SkyDescribe the 1st alert \
+842 = cmd,/usr/local/bin/SkywarnPlus/SkyDescribe.py 2 ; SkyDescribe the 2nd alert \
+843 = cmd,/usr/local/bin/SkywarnPlus/SkyDescribe.py 3 ; SkyDescribe the 3rd alert \
+844 = cmd,/usr/local/bin/SkywarnPlus/SkyDescribe.py 4 ; SkyDescribe the 4th alert \
+845 = cmd,/usr/local/bin/SkywarnPlus/SkyDescribe.py 5 ; SkyDescribe the 5th alert \
+846 = cmd,/usr/local/bin/SkywarnPlus/SkyDescribe.py 6 ; SkyDescribe the 6th alert \
+847 = cmd,/usr/local/bin/SkywarnPlus/SkyDescribe.py 7 ; SkyDescribe the 7th alert \
+848 = cmd,/usr/local/bin/SkywarnPlus/SkyDescribe.py 8 ; SkyDescribe the 8th alert \
+849 = cmd,/usr/local/bin/SkywarnPlus/SkyDescribe.py 9 ; SkyDescribe the 9th alert' "$CONF_FILE"
 
-# Add the cron job and comment to the root user's crontab
-(sudo crontab -l 2>/dev/null; echo "$CRON_COMMENT"; echo "$CRON_JOB") | sudo crontab -
+	sed -i '/tailmessagetime=/s/^#//; s/tailmessagetime=.*/tailmessagetime=600000/' "$CONF_FILE"
+	sed -i '/tailsquashedtime=/s/^#//; s/tailsquashedtime=.*/tailsquashedtime=30000/' "$CONF_FILE"
+	awk '/tailsquashedtime=30000/ { print "tailmessagelist = /tmp/SkywarnPlus/wx-tail"; } { print }' "$CONF_FILE" > "${CONF_FILE}.tmp" && mv "${CONF_FILE}.tmp" "$CONF_FILE"
+	
+	echo "Skywarnplus installation complete."
+}
 
-# Print the current crontab to verify
-echo "Current crontab for root:"
-sudo crontab -l
+install_dvswitch() {
+	echo "Installing dvswitch..."
+	# Install deps before we install dvswitch
+	apt install -y php-cgi libapache2-mod-php8.2
+	cd /tmp || exit
+	wget dvswitch.org/bookworm
+	chmod +x bookworm
+	./bookworm
+	apt update
+	apt install -y dvswitch-server
 
+	# Allow Apache2 to access /tmp
+	cp /lib/systemd/system/apache2.service /etc/systemd/system/
+	sed -i 's/true/false/g' /etc/systemd/system/apache2.service
+	systemctl restart apache2
+	
+	echo "Dvswitch Server installation complete."
+}
 
+# Parse command-line arguments
+while getopts "aswdh" opt; do
+	case $opt in
+		a)
+			install_allscan_flag=true
+		;;
+		s)
+			install_supermon_flag=true
+		;;
+		w)
+			install_skywarnplus_flag=true
+		;;
+		d)
+			install_dvswitch_flag=true
+		;;
+		h)
+			usage
+			exit 0
+		;;
+		*)
+			usage
+			exit 1
+		;;
+	esac
+done
 
-echo "All installations and configurations are completed."
+# If no options are provided, show usage and exit
+if [ "$OPTIND" -eq 1 ]; then
+	usage
+	exit 1
+fi
+
+# Perform the installations based on flags
+[ "$install_allscan_flag" ] && install_allscan
+[ "$install_supermon_flag" ] && install_supermon
+[ "$install_skywarnplus_flag" ] && install_skywarnplus
+[ "$install_dvswitch_flag" ] && install_dvswitch
